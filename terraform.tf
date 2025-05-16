c57ad8d7648215183dcb4ac18d944056c2300b6b
sirishameduri <sirisha.meduri@gmail.com>
2025-05-16 10:06:33 -0400

    origin

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "~> 1.0"
+  backend "s3" {
+    bucket = "aws-largescaleproject-terraform"
+    key    = "gitops-actions/state.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.16"
+    }
+  }
+}
